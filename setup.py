b2b1a5f0625ea13933142f535356d4e2cd70b098
Kirill Sulim <kirillsulim@gmail.com>
2019-04-26 16:51:55 +0500

    Add README to distribution

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
+from os import path
 
+
+project_directory = path.abspath(path.dirname(__file__))
+
+
+def load_from(file_name):
+    with open(path.join(project_directory, file_name), encoding=utf_8.getregentry().name) as f:
+        return f.read()
 
 setup(
     name='gitsnapshot',
     version='0.1.1',
     description='Python module to simplify loading of snapshot of git repository',
+    long_description=load_from('README.md'),
+    long_description_content_type='text/markdown',
     author='Kirill Sulim',
     author_email='kirillsulim@gmail.com',
     license='MIT',
