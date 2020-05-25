4db29006c9758c1d1bb112c7c769c2f0404f9525
Kirill Sulim <kirillsulim@gmail.com>
2020-05-25 22:15:35 +0500

    Add style and some typing

--- gitsnapshot.py
+++ gitsnapshot.py
@@ -1,16 +1,16 @@
-from typing import Optional, Tuple
-from enum import Enum, unique
-from subprocess import call, run, PIPE
-from pathlib import Path
-import sys
 import shutil
+import sys
+from enum import Enum, unique
+from pathlib import Path
+from subprocess import PIPE, call, run
+from typing import Optional, Tuple, Union
 
 
 @unique
 class CheckoutType(Enum):
-    BRANCH = 'branch'
-    TAG = 'tag'
-    COMMIT = 'commit'
+    BRANCH = "branch"
+    TAG = "tag"
+    COMMIT = "commit"
 
 
 class CheckoutCommand:
@@ -19,16 +19,16 @@ class CheckoutCommand:
         self.value = value
 
 
-DEFAULT_COMMAND = CheckoutCommand(CheckoutType.BRANCH, 'master')
+DEFAULT_COMMAND = CheckoutCommand(CheckoutType.BRANCH, "master")
 
 
 def load_repo(
-        path: str,
-        repo_url: str,
-        branch: str = None,
-        tag: str = None,
-        commit: str = None,
-        use_existing: bool = False
+    path: Union[Path, str],
+    repo_url: str,
+    branch: str = None,
+    tag: str = None,
+    commit: str = None,
+    use_existing: bool = False,
 ) -> Optional[str]:
     """
     Load repository from "repo_url" into "path" folder. Only one parameter from "branch", "tag" or "commit" must be
@@ -51,13 +51,13 @@ def load_repo(
 
     if path.exists() and not _dir_is_empty(path):
         if not use_existing:
-            return 'Use of existing repository is forbidden'
+            return "Use of existing repository is forbidden"
 
         actual_origin, error = _remote_url(path)
         if error:
             return error
         if actual_origin != repo_url:
-            return 'Path {} expected to contain {} repo but was {}'.format(path, repo_url, actual_origin)
+            return "Path {} expected to contain {} repo but was {}".format(path, repo_url, actual_origin)
 
         return checkout_existing_repo(path, command)
 
@@ -66,74 +66,76 @@ def load_repo(
 
 
 def checkout_existing_repo(path: Path, checkout: CheckoutCommand) -> Optional[str]:
-    git_dir = str(path.joinpath('.git'))
+    git_dir = str(path.joinpath(".git"))
     path = str(path)
 
-    code = call(['git', '--git-dir', git_dir, '--work-tree', path, 'fetch', '--unshallow'])
+    code = call(["git", "--git-dir", git_dir, "--work-tree", path, "fetch", "--unshallow"])
     if code:
-        code = call(['git', '--git-dir', git_dir, '--work-tree', path, 'fetch'])
+        code = call(["git", "--git-dir", git_dir, "--work-tree", path, "fetch"])
     if code:
-        return 'git fetch exited with code {}'.format(code)
+        return "git fetch exited with code {}".format(code)
 
     if checkout.type is CheckoutType.BRANCH:
-        code = call(['git', '--git-dir', git_dir, '--work-tree', path, 'checkout', 'origin/{}'.format(checkout.value)])
+        code = call(["git", "--git-dir", git_dir, "--work-tree", path, "checkout", checkout.value])
+        if code == 0:
+            code = call(["git", "--git-dir", git_dir, "--work-tree", path, "pull"])
     elif checkout.type in [CheckoutType.TAG, CheckoutType.COMMIT]:
-        code = call(['git', '--git-dir', git_dir, '--work-tree', path, 'checkout', checkout.value])
+        code = call(["git", "--git-dir", git_dir, "--work-tree", path, "checkout", checkout.value])
     else:
-        return 'Unknown checkout type {}'.format(checkout.type)
+        return "Unknown checkout type {}".format(checkout.type)
     if code:
-        return 'git fetch exited with code {}'.format(code)
+        return "git fetch exited with code {}".format(code)
 
 
 def checkout_new_repo(path: Path, url: str, checkout: CheckoutCommand) -> Optional[str]:
     path.mkdir(parents=True, exist_ok=True)
-    git_dir = str(path.joinpath('.git'))
+    git_dir = str(path.joinpath(".git"))
     path = str(path)
 
     if checkout.type in [CheckoutType.BRANCH, CheckoutType.TAG]:
-        code = call(['git', 'clone', '--branch', checkout.value, '--depth', '1', url, path])
+        code = call(["git", "clone", "--branch", checkout.value, "--depth", "1", url, path])
         if code:
-            return 'git clone exited with code {}'.format(code)
+            return "git clone exited with code {}".format(code)
     elif checkout.type is CheckoutType.COMMIT:
-        code = call(['git', 'clone', url, path])
+        code = call(["git", "clone", url, path])
         if code:
-            return 'git clone exited with code {}'.format(code)
-        code = call(['git', '--git-dir', git_dir, '--work-tree', path, 'checkout', checkout.value])
+            return "git clone exited with code {}".format(code)
+        code = call(["git", "--git-dir", git_dir, "--work-tree", path, "checkout", checkout.value])
         if code:
-            return 'git checkout exited with code {}'.format(code)
+            return "git checkout exited with code {}".format(code)
     else:
-        return 'Unknown checkout type {}'.format(checkout.type)
+        return "Unknown checkout type {}".format(checkout.type)
 
 
 def _extract_fetch_type(branch: str, tag: str, commit: str) -> Tuple[Optional[CheckoutCommand], Optional[str]]:
-    values = list(filter(
-        lambda c: c.value is not None,
-        [
-            CheckoutCommand(CheckoutType.BRANCH, branch),
-            CheckoutCommand(CheckoutType.TAG, tag),
-            CheckoutCommand(CheckoutType.COMMIT, commit)
-        ]
-    ))
+    values = list(
+        filter(
+            lambda c: c.value is not None,
+            [
+                CheckoutCommand(CheckoutType.BRANCH, branch),
+                CheckoutCommand(CheckoutType.TAG, tag),
+                CheckoutCommand(CheckoutType.COMMIT, commit),
+            ],
+        )
+    )
 
     if len(values) == 0:
         return DEFAULT_COMMAND, None
     elif len(values) != 1:
-        return None, 'Too many checkout parameters: {}'.format(values)
+        return None, "Too many checkout parameters: {}".format(values)
 
     return values[0], None
 
 
 def _remote_url(repo_dir: Path) -> Tuple[Optional[str], Optional[str]]:
-    git_dir = str(repo_dir.joinpath('.git'))
+    git_dir = str(repo_dir.joinpath(".git"))
     repo_dir = str(repo_dir.as_posix())
 
     result = run(
-        ['git', '--git-dir', git_dir, '--work-tree', repo_dir, 'remote', 'get-url', 'origin'],
-        stdout=PIPE,
-        stderr=PIPE
+        ["git", "--git-dir", git_dir, "--work-tree", repo_dir, "remote", "get-url", "origin"], stdout=PIPE, stderr=PIPE
     )
     if len(result.stderr) == 0 and len(result.stdout) != 0:
-        return result.stdout.decode(sys.stdout.encoding).strip('\n '), None
+        return result.stdout.decode(sys.stdout.encoding).strip("\n "), None
     else:
         return None, result.stderr.decode(sys.stderr.encoding)
 
@@ -143,8 +145,8 @@ def _dir_is_empty(directory: Path) -> bool:
 
 
 def _git_version() -> Optional[str]:
-    if shutil.which('git'):
-        result = run(['git', '--version'], stdout=PIPE)
-        return result.stdout.decode(sys.stdout.encoding).strip('\n ').split(' ')[-1]
+    if shutil.which("git"):
+        result = run(["git", "--version"], stdout=PIPE)
+        return result.stdout.decode(sys.stdout.encoding).strip("\n ").split(" ")[-1]
     else:
         return None
--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,48 @@
+from pathlib import Path
+
 import nox
+
+LINE_LENGTH = 120
+FLAKE8_IGNORE = [
+    "E203",
+    "E231",
+    "W503",
+]
 
 
 @nox.session
 def tests(session):
-    session.install('.')
-    session.install('pytest')
-    session.run('pytest', 'tests.py')
+    session.install(".")
+    session.install("pytest")
+    session.run("pytest", "tests.py")
 
 
 @nox.session
 def lint(session):
-    session.install('flake8')
-    session.run('flake8', 'gitsnapshot.py', 'tests.py')
+    session.install("flake8", "black", "isort")
+
+    source_files = list(map(str, Path(".").glob("*.py")))
+
+    session.run("black", "--target-version", "py38", "--line-length", f"{LINE_LENGTH}", "--check", *source_files)
+    session.run(
+        "flake8",
+        "--max-line-length",
+        f"{LINE_LENGTH}",
+        "--extend-ignore",
+        ",".join(FLAKE8_IGNORE),
+        "--show-source",
+        *source_files,
+    )
+    session.run(
+        "isort",
+        "--multi-line",
+        "3",
+        "--trailing-comma",
+        "--force-grid-wrap",
+        "0",
+        "--use-parentheses",
+        "--line-width",
+        f"{LINE_LENGTH}",
+        "--check-only",
+        *source_files,
+    )
